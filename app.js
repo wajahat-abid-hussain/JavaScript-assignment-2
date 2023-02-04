20e10421af2a7795dafa1abd37afa5ecb4007af0
Wajahat Hussain <51961119+wajahat-abid-hussain@users.noreply.github.com>
2023-02-04 20:16:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//Chapter 2
+
+// ==================Question NO: 1===========
+var username;
+// ==================Question NO: 2===========
+var myname= "wajahat";
+// ==================Question NO: 3===========
+var msg= "Hello World";
+alert(msg)
+
+// ==================Question NO: 4===========
+var name= "waji";
+var age= "28";
+var edu= "Software Developer";
+alert(name)        
+alert(age)        
+alert(edu)        
+// ==================Question NO: 5===========
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+// ==================Question NO: 6===========
+var email="waji@gmail.com"
+alert("My email address is "+email)
+// ==================Question NO: 7===========
+var book="smart way to learn javascript"
+alert("I am trying to learn from the book "+book)
+// ==================Question NO: 8===========
+document.write("Yah! I can write HTML content by javascript")
+// ==================Question NO: 9===========
+var variable = "▬▬▬▬▬▬▬▬▬▬▬▬▬"
+alert(variable)
+
+
+//chapter3
+ //    ===============Question No:1==============
+ var age = 25
+ alert("i am " + age + " years old")
+
+ //    ===============Question No:2==============
+ var num = 2
+ alert("you have visited this site "+ num +" times")
+    
+ //    ===============Question No:2==============
+ var date = 1997 
+ document.write("My birth year is "+ date)
+ document.write("<br>"+"Data type of my birth year is "+ typeof(date))
+
+ //    ===============Question No:2==============
+ var visName = "Kareem"
+ var pro = "T-shirt"
+ var ord = 4
+ document.write("<br><br>"+"<b>"+visName+"</b>"+" ordered "+"<b>"+ord+" "+pro+"</b>"+"on www.mskmart.com store") 
+ 
+
+
+ //chapter 4
+    // ===========Question No: 1================
+    var b, c, d;
+    
+    // ===========Question No: 2================
+    var name;
+    var $name;
+    var name2;
+    var _name;
+    var name2nd;
+    // var !name;
+    // var 2name;
+    // var var;
+    // var first name;
+    // var Sec%;
+
+    // ===========Question No: 3================
+    document.write("<p>"+"<h1>"+"Question NO:3"+"</h1>"+"</p>")
+    document.write("<p>"+"<h1>"+"Rules for naming JS variables"+"</h1>"+"</p>")
+    document.write("<p>"+"Variable names can only contain $, _, letter and number"+"</p>")
+    document.write("<p>"+"Variable must begin with a $, _ or letter "+"</p>")
+    document.write("<p>"+"Variable names are case sensitive"+"</p>")
+    document.write("<p>"+"Variable names should not be JS keyword"+"</p>")
+
